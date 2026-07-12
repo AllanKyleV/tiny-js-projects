551803108f8e9bbbf7779d688833fb134398295d
Allan Kyle <199327511+AllanKyleV@users.noreply.github.com>
2026-07-12 13:28:06 +0800

    1st project on storage and blob

--- notes-box/scripts.js
+++ notes-box/scripts.js
@@ -1,34 +1,37 @@
-const inputNote = document.querySelector("#input-note");
-const addBtn = document.querySelector("#add-btn");
-const retrieveBtn = document.querySelector("#retrieve-btn");
-const list = document.querySelector("#display-note");
+const inputNote = document.querySelector('#input-note');
+const addBtn = document.querySelector('#add-btn');
+const retrieveBtn = document.querySelector('#retrieve-btn');
+const list = document.querySelector('#display-note');
+const clear = document.querySelector('#clear-btn');
+const downloadNote = document.querySelector('#download-note');
+const a = document.querySelector('a');
 
-// // Add note, display when retrieved in clicked.
-// addBtn.addEventListener("click", () => {
+// This is our notes.
+let notes = JSON.parse(localStorage.getItem('notes')) || [];
 
-//     // To set a key value pairs.
-//     localStorage.setItem("notes", inputNote.value);
-//     inputNote.value = "";
-// })
-
-// retrieveBtn.addEventListener("click", () => {
-//     // To retrieve the key.
-//     list.textContent = localStorage.getItem("notes")
-// })
-
-// Set an array to be stored.
-// Add items to array from input as add button clicked.
-// convert the array as an array of storage items using JSON and stringify.
-// Read/retrieve the array by parsing.
-
-let notes = JSON.parse(localStorage.getItem("notes")) || [];
-
-addBtn.addEventListener("click", () => {
-    if (inputNote.value !== "") notes.push(inputNote.value);
-    localStorage.setItem("notes", JSON.stringify(notes));
-    inputNote.value = "";
+addBtn.addEventListener('click', () => {
+  if (inputNote.value !== '') {
+    notes.push(inputNote.value);
+  }
+  localStorage.setItem('notes', JSON.stringify(notes));
+  inputNote.value = '';
 })
 
-retrieveBtn.addEventListener("click", () => {
-    list.textContent =  JSON.parse(localStorage.getItem("notes"));
+retrieveBtn.addEventListener('click', () => {
+  list.textContent = JSON.parse(localStorage.getItem('notes'));
 })
+
+clear.addEventListener('click', () => {
+  localStorage.clear();
+})
+
+// Blob.
+// Mini-project: Add a Download Note button that saves the note from the previous project as a .txt file.
+downloadNote.addEventListener('click', () => {
+  const blob = new Blob([notes.join('\n')], { type: 'text/plain' });
+  const url = URL.createObjectURL(blob);
+  downloadNote.href = url;
+  downloadNote.download = 'message.txt';
+});
+
+// 
